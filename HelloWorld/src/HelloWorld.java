4e8bb7d185aaffe4c8bea47202644bac6dbec18f
BIKARTANA <bpanigrahy@yahoo.co.in>
2015-04-21 14:28:56 +0530

    Comment updated

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello. Tryout your ant build by uploading to git 3rd time");
+System.out.println("Hello world. ");
 	}
 
 }
